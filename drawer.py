be0e18e3a395e8132e871e8fbcb57e2e4d98d280
MuleHakim <mulukenhakim97@gmail.com>
2022-06-16 14:17:53 +0300

    comment adjestment

--- drawer.py
+++ drawer.py
@@ -34,7 +34,6 @@ void main()
 """
 
 
-# glfw callback functions
 
 
 def window_resize(window, width, height):
@@ -43,32 +42,26 @@ def window_resize(window, width, height):
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
 
 
-# initializing glfw library
 if not glfw.init():
     raise Exception("glfw can not be initialized!")
 
-# creating the window
 window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
 
-# check if window was created
 if not window:
     glfw.terminate()
     raise Exception("glfw window can not be created!")
 
-# set window's position
 glfw.set_window_pos(window, 400, 200)
 
-# set the callback function for window resize
 glfw.set_window_size_callback(window, window_resize)
 
-# make the context current
 glfw.make_context_current(window)
 
-# load here the 3d meshes
-chibi_indices, chibi_buffer = ObjLoader.load_model("road.obj")
-monkey_indices, monkey_buffer = ObjLoader.load_model("floor.obj")
-solarpanel_indices, solarpanel_buffer = ObjLoader.load_model("solarpanelobj.obj")
-tree_indices, tree_buffer = ObjLoader.load_model("tree.obj")
+
+road_verts, road_buff = ObjLoader.load_model("road.obj")
+floor_verts, floor_buff = ObjLoader.load_model("floor.obj")
+solarpanel_verts, solarpanel_buffer = ObjLoader.load_model("solarpanelobj.obj")
+tree_verts, tree_verts = ObjLoader.load_model("tree.obj")
 home_indices, home_buffer = ObjLoader.load_model("home.obj")
 door_indices, door_buffer = ObjLoader.load_model("door.obj")
 window_indices, window_buffer = ObjLoader.load_model("window.obj")
@@ -82,221 +75,216 @@ sun_indices, sun_buffer = ObjLoader.load_model("sun.obj")
 
 shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
 
-# VAO and VBO
+
 VAO = glGenVertexArrays(13)
 VBO = glGenBuffers(13)
-# EBO = glGenBuffers(1)
 
-# Chibi VAO
+
+# road
 glBindVertexArray(VAO[0])
-# Chibi Vertex Buffer Object
 glBindBuffer(GL_ARRAY_BUFFER, VBO[0])
-glBufferData(GL_ARRAY_BUFFER, chibi_buffer.nbytes, chibi_buffer, GL_STATIC_DRAW)
+glBufferData(GL_ARRAY_BUFFER, road_buff.nbytes, road_buff, GL_STATIC_DRAW)
 
-# glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-# glBufferData(GL_ELEMENT_ARRAY_BUFFER, chibi_indices.nbytes, chibi_indices, GL_STATIC_DRAW)
 
-# chibi vertices
+
 glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, chibi_buffer.itemsize * 8, ctypes.c_void_p(0))
-# chibi textures
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, road_buff.itemsize * 8, ctypes.c_void_p(0))
+
 glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, chibi_buffer.itemsize * 8, ctypes.c_void_p(12))
-# chibi normals
-glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, chibi_buffer.itemsize * 8, ctypes.c_void_p(20))
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, road_buff.itemsize * 8, ctypes.c_void_p(12))
+
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, road_buff.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(4)
 
-# Monkey VAO
+# floor 
 glBindVertexArray(VAO[1])
-# Monkey Vertex Buffer Object
-glBindBuffer(GL_ARRAY_BUFFER, VBO[1])
-glBufferData(GL_ARRAY_BUFFER, monkey_buffer.nbytes, monkey_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+glBindBuffer(GL_ARRAY_BUFFER, VBO[1])
+glBufferData(GL_ARRAY_BUFFER, floor_buff.nbytes, floor_buff, GL_STATIC_DRAW)
+
+
 glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, floor_buff.itemsize * 8, ctypes.c_void_p(0))
+
 glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
-glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(20))
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, floor_buff.itemsize * 8, ctypes.c_void_p(12))
+
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, floor_buff.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(4)
 
-# Monkey VAO
+# solar_panel 
 glBindVertexArray(VAO[2])
-# Monkey Vertex Buffer Object
 glBindBuffer(GL_ARRAY_BUFFER, VBO[2])
 glBufferData(GL_ARRAY_BUFFER, solarpanel_buffer.nbytes, solarpanel_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, solarpanel_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, solarpanel_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, solarpanel_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(4)
 
-# Monkey VAO
+# tree
 glBindVertexArray(VAO[3])
-# Monkey Vertex Buffer Object
-glBindBuffer(GL_ARRAY_BUFFER, VBO[3])
-glBufferData(GL_ARRAY_BUFFER, tree_buffer.nbytes, tree_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+glBindBuffer(GL_ARRAY_BUFFER, VBO[3])
+glBufferData(GL_ARRAY_BUFFER, tree_verts.nbytes, tree_verts, GL_STATIC_DRAW)
+
+
 glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, tree_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, tree_verts.itemsize * 8, ctypes.c_void_p(0))
+
 glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, tree_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
-glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, tree_buffer.itemsize * 8, ctypes.c_void_p(20))
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, tree_verts.itemsize * 8, ctypes.c_void_p(12))
+
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, tree_verts.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(4)
 
-# Monkey VAO
+# home
 glBindVertexArray(VAO[4])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[4])
 glBufferData(GL_ARRAY_BUFFER, home_buffer.nbytes, home_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, home_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, home_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, home_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(5)
 
-# Monkey VAO
+# door
 glBindVertexArray(VAO[5])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[5])
 glBufferData(GL_ARRAY_BUFFER, door_buffer.nbytes, door_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, door_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, door_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, door_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(6)
 
-# Monkey VAO
+# window
 glBindVertexArray(VAO[6])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[6])
 glBufferData(GL_ARRAY_BUFFER, window_buffer.nbytes, window_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, window_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, window_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, window_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(7)
 
-# Monkey VAO
+# hotel
 glBindVertexArray(VAO[7])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[7])
 glBufferData(GL_ARRAY_BUFFER, hotel_buffer.nbytes, hotel_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, hotel_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, hotel_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, hotel_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(8)
 
-# Monkey VAO
+# window2
 glBindVertexArray(VAO[8])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[8])
 glBufferData(GL_ARRAY_BUFFER, window2_buffer.nbytes, window2_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, window2_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, window2_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, window2_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(9)
 
-# Monkey VAO
+# hotel2
 glBindVertexArray(VAO[9])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[9])
 glBufferData(GL_ARRAY_BUFFER, hotel2_buffer.nbytes, hotel2_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, hotel2_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, hotel2_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, hotel2_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(10)
 
-# Monkey VAO
+# hotel3
 glBindVertexArray(VAO[10])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[10])
 glBufferData(GL_ARRAY_BUFFER, hotel3_buffer.nbytes, hotel3_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, hotel3_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, hotel3_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, hotel3_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(11)
 
-# Monkey VAO
+# solarpanel leg
 glBindVertexArray(VAO[11])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[11])
 glBufferData(GL_ARRAY_BUFFER, leg_buffer.nbytes, leg_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, leg_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, leg_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, leg_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(12)
 
 
 
-# Monkey VAO
+# sun
 glBindVertexArray(VAO[12])
-# Monkey Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[12])
 glBufferData(GL_ARRAY_BUFFER, sun_buffer.nbytes, sun_buffer, GL_STATIC_DRAW)
 
-# monkey vertices
+
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sun_buffer.itemsize * 8, ctypes.c_void_p(0))
-# monkey textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sun_buffer.itemsize * 8, ctypes.c_void_p(12))
-# monkey normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sun_buffer.itemsize * 8, ctypes.c_void_p(20))
 glEnableVertexAttribArray(13)
 
@@ -340,7 +328,6 @@ hotel3_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -5, -10]))
 leg_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -5, -10]))
 sun_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -5, -10]))
 
-# eye, target, up
 view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 0, 8]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 1, 0]))
 
 model_loc = glGetUniformLocation(shader, "model")
@@ -350,7 +337,6 @@ view_loc = glGetUniformLocation(shader, "view")
 glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
 glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
 
-# the main application loop
 while not glfw.window_should_close(window):
     glfw.poll_events()
 
@@ -359,29 +345,28 @@ while not glfw.window_should_close(window):
     rot_y = pyrr.Matrix44.from_y_rotation(0.1 * glfw.get_time())
     model = pyrr.matrix44.multiply(rot_y, chibi_pos)
 
-    # draw the chibi character
     glBindVertexArray(VAO[0])
     glBindTexture(GL_TEXTURE_2D, textures[0])
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-    glDrawArrays(GL_TRIANGLES, 0, len(chibi_indices))
+    glDrawArrays(GL_TRIANGLES, 0, len(road_verts))
 
     model = pyrr.matrix44.multiply(rot_y, monkey_pos)
     glBindVertexArray(VAO[1])
     glBindTexture(GL_TEXTURE_2D, textures[1])
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-    glDrawArrays(GL_TRIANGLES, 0, len(monkey_indices))
+    glDrawArrays(GL_TRIANGLES, 0, len(floor_verts))
 
     model = pyrr.matrix44.multiply(rot_y, solarpanel_pos)
     glBindVertexArray(VAO[2])
     glBindTexture(GL_TEXTURE_2D, textures[2])
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-    glDrawArrays(GL_TRIANGLES, 0, len(solarpanel_indices))
+    glDrawArrays(GL_TRIANGLES, 0, len(solarpanel_verts))
 
     model = pyrr.matrix44.multiply(rot_y, tree_pos)
     glBindVertexArray(VAO[3])
     glBindTexture(GL_TEXTURE_2D, textures[3])
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-    glDrawArrays(GL_TRIANGLES, 0, len(tree_indices))
+    glDrawArrays(GL_TRIANGLES, 0, len(tree_verts))
 
     model = pyrr.matrix44.multiply(rot_y, home_pos)
     glBindVertexArray(VAO[4])
